1cf90cae57055a4c93831eba0d8b0a0f09b0aa16
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-03-29 17:06:00 +0530

    comments on category class
    java doc comments added

--- src/com/mastermindapps/Categories.java
+++ src/com/mastermindapps/Categories.java
@@ -1,24 +1,46 @@
-package com.mastermindapps.DataTypes;
+package com.mastermindapps;
 
 class Categories {
     private String categoryName;
     private String categoryParent;
     private int discount;
 
+    /**
+     * Constructor for category class.
+     *
+     * @param categoryName   name of the current category
+     * @param categoryParent name of current category's direct parent.
+     * @param discount       discount associated with current category irrespective of parent.
+     */
     Categories(String categoryName, String categoryParent, int discount) {
         this.categoryName = categoryName;
         this.categoryParent = categoryParent;
         this.discount = discount;
     }
 
+    /**
+     * Getter method for current category name
+     *
+     * @return string name of category
+     */
     String getCategoryName() {
         return categoryName;
     }
 
+    /**
+     * Getter method for initalized category's parent
+     *
+     * @return string name of category parent
+     */
     String getCategoryParent() {
         return categoryParent;
     }
 
+    /**
+     * Getter method for initiazed category's discount
+     *
+     * @return integer value of category's discount
+     */
     int getDiscount() {
         return discount;
     }
