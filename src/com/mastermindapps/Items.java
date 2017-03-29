ad2593a25424533fffdf36ccf0428da4cbea7be6
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-03-29 17:12:30 +0530

    java doc comments for items class

--- src/com/mastermindapps/Items.java
+++ src/com/mastermindapps/Items.java
@@ -6,6 +6,14 @@ class Items {
     private String category;
     private int price;
 
+    /**
+     * Constructor for Items class.
+     *
+     * @param id        int value for the row number of the inventory list.
+     * @param brandName string value of the brand name.
+     * @param category  string value of the category.
+     * @param price     integer value of the original pre discount price of the item.
+     */
     Items(int id, String brandName, String category, int price) {
         this.id = id;
         this.brandName = brandName;
@@ -13,18 +21,38 @@ class Items {
         this.price = price;
     }
 
+    /**
+     * Getter method for retrieveing id value.
+     *
+     * @return integer value of id of current item.
+     */
     int getId() {
         return id;
     }
 
+    /**
+     * Getter method for retreiving brand's name.
+     *
+     * @return string value of brand of current item.
+     */
     String getBrandName() {
         return brandName;
     }
 
+    /**
+     * Getter method for retrieving category's name.
+     *
+     * @return string value of category of current item.
+     */
     String getCategory() {
         return category;
     }
 
+    /**
+     * Getter method for retrieving pre discount price.
+     *
+     * @return integer value of original price.
+     */
     int getPrice() {
         return price;
     }
